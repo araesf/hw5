d403f2b7d3c47b9702c536e91d4d0e1a87db2b01
Ara Esfarjani <aesfarja@usc.edu>
2025-04-25 00:48:30 +0000

    curr version

--- schedwork.cpp
+++ schedwork.cpp
@@ -16,14 +16,59 @@ using namespace std;
 
 // a constant that can be used to indicate an INVALID 
 // worker ID if that is useful to your implementation.
+
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
+
+// type for the ID of a worker
+typedef unsigned int Worker_T;
+
+// n-by-k Matrix of each of the k workers' availability over an n-day period
+
+typedef std::vector<std::vector<bool>> AvailabilityMatrix;
+
+// n-by-d matrix with the d worker IDs who are scheduled 
+// to work on each of the n days
+typedef std::vector<std::vector<Worker_T> > DailySchedule;
 
 
 // Add prototypes for any helper functions here
 
 
 // Add your implementation of schedule() and other helper functions here
+bool schedule_bt(const AvailabilityMatrix& avail, const size_t dailyNeed, 
+const size_t maxShifts, DailySchedule& sched, vector<int>& workerFreq, int col, int row) {
+
+	if (row == avail.size()) {
+		return true;
+	}
+
+	if (col == dailyNeed) {
+		return schedule_bt(avail, dailyNeed, maxShifts, sched, workerFreq, 0, row + 1);
+	}
+
+	// i is the given worker
+	for (int i = 0; i < avail[0].size(); i++) {
+		// if worker is available and hasnt achieved maxShifts yet
+		if (avail[row][i] == true && workerFreq[i] < maxShifts) {
+			workerFreq[i]++;
+			sched[row][col] = i;
+
+			// with the current selection, test if true. if row == avail.size(), return true, else undo
+			if (schedule_bt(avail, dailyNeed, maxShifts, sched, workerFreq, col + 1, row)) {
+				return true;
+			}
+
+			// undo
+			workerFreq[i]--;
+		}
+	}
+
+	return false;
+}
+
+
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -37,9 +82,19 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+		int row = 0;
+		int col = 0;
+		vector<int> workerFreq(avail[0].size(), 0);
 
+		// populate schedule 2D
+		for (int r = 0; r < avail.size(); r++) {
+			vector<Worker_T> curr_day;
+			for (int c = 0; c < dailyNeed; c++) {
+				curr_day.push_back(INVALID_ID);
+			}
 
+			sched.push_back(curr_day);
+		}
 
-
+	return schedule_bt(avail, dailyNeed, maxShifts, sched, workerFreq, col, row);
 }
-
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordle_helper(std::string& inlol, std::string floating, const std::set<std::string>& dict, int i, set<string>& valid_set);
+// bool floating_included(std::string& inlol, const std::string& floating);
 
 
 // Definition of primary wordle function
@@ -21,8 +23,92 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
+		std::set<string> valid_set;
+		std::string inlol = in;
+        std::string floatin = floating;
+
     // Add your code here
+    wordle_helper(inlol, floatin, dict, 0, valid_set);
+    return valid_set;
 
 }
 
 // Define any helper functions here
+void wordle_helper(std::string& inlol, std::string floating, const std::set<std::string>& dict, int i, std::set<string>& valid_set) {
+    // base case. if correct size, valid english word, and all floating chars are included
+    if (inlol.size() == i && dict.find(inlol) != dict.end() && floating.empty()) {
+        valid_set.insert(inlol);
+		return;
+    }
+
+    // to prune, you have to shrink the floating string when the character is used
+    int rem_hyphens = 0;
+    for (int h = i; h < inlol.size(); h++) {
+        if (inlol[h] == '-') {
+            rem_hyphens += 1;
+        }
+    }
+
+	if (i >= inlol.size()) {
+		return;
+	}
+
+    // if there are not enough spaces for the floating characters, it is invalid
+    if (rem_hyphens < floating.size()) {
+        return;
+    }
+
+    // if inlol is a given character, we must still shrink the floating string to account for this character if it is in the string
+    // do not need to make copy as this is part of the inital in string and will apply to all recursive calls
+	if (inlol[i] != '-') {
+        if (floating.find(inlol[i]) != std::string::npos) {
+            floating.erase(floating.find(inlol[i]), 1);
+        }
+
+		wordle_helper(inlol, floating, dict, i + 1, valid_set);
+		return;
+	} 
+
+    // test every character at the position
+    for (char c = 'a'; c <= 'z'; c++) {
+        inlol[i] = c;
+
+        std::string floating_copy = floating;
+        
+        if (floating_copy.find(c) != std::string::npos) {
+            floating_copy.erase(floating_copy.find(c), 1);
+        }
+
+    	// recurse onwords (skip character)
+		wordle_helper(inlol, floating_copy, dict, i + 1, valid_set);
+
+        // reverse the change
+	    inlol[i] = '-';
+	}
+}
+
+// discarding due to runtime constraints:
+// bool floating_included(std::string& inlol, const std::string& floating) {
+//     std::map<char, int> f_count;
+//     std::map<char, int> in_count;
+
+//     // check for same frequency of characters in both in and the floating 
+//     // (if floating is aa, then we need 2 a's. inlol must have a count have 2 a's at the key 'a')
+
+//     for (char c : floating) {
+//         f_count[c] += 1;
+//     }
+
+//     for (char i : inlol) {
+//         in_count[i] += 1;
+//     }
+
+//     // now iterate over the f_count and if key in in_count has a count that is less that f_count, then return false
+//     for (const std::pair<char, int>& m : f_count) {
+//         if (in_count[m.first] < m.second) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
